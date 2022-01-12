52dbc1cbe17838d708f06b946c801a440fda88cc
sonercxt <soner.cxtdemir@gmail.com>
2022-01-12 11:53:59 +0100

    powercoders assignmnet is done

---
+++ app.js
@@ -0,0 +1,7 @@
+let sendMail = () => {
+    document.getElementById("form-name").innerHTML = ""
+    document.getElementById("exampleInputEmail1").innerHTML = ""
+    document.getElementById("form-subject").innerHTML = ""
+    document.getElementById("form-message").innerHTML = ""
+
+}
